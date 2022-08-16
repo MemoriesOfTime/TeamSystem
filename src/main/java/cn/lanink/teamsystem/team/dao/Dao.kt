2b84f6dfc5361bd9fef99c29bc0c4276d8af00bc
LT_Name <572413378@qq.com>
2022-08-16 21:29:43 +0800

    添加Dao#isAllMemberOnline()方法

--- src/main/java/cn/lanink/teamsystem/team/dao/Dao.kt
+++ src/main/java/cn/lanink/teamsystem/team/dao/Dao.kt
@@ -13,10 +13,20 @@ interface Dao {
     val maxPlayers: Int
     val name: String
 
+    /**
+     * 判断玩家是否是队长
+     */
     fun isTeamLeader(other: Player): Boolean
     fun isTeamLeader(other: String): Boolean
 
+    /**
+     * 设置队伍队长
+     */
     fun setTeamLeader(leader: Player)
+
+    /**
+     * 获取队伍队长
+     */
     fun getTeamLeader(): Player?
 
     fun addPlayer(player: Player)
@@ -31,7 +41,17 @@ interface Dao {
     fun cancelApplyFrom(player: Player)
     fun cancelApplyFrom(playerName: String)
 
+    /**
+     * 判断玩家是否在线（当前服务器）
+     */
     fun isMemberOnline(playerName: String): Boolean
+
+    /**
+     * 判断队伍中的所有玩家是否在线（当前服务器）
+     */
+    fun isAllMemberOnline(): Boolean {
+        return players.all { isMemberOnline(it) }
+    }
 
     fun getMemberLoginAt(playerName: String): String
 
